7c9c13cf5028218c25c7d24d0120b75867f7cdcb
Nikolai Kristiansen <nikolaik@gmail.com>
2014-05-26 13:49:11 +0200

    Missing server reqs

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express'),
+    logger = require('morgan'),
     http = require('http');
 
-var app = express()
-    .use(express.logger('dev'))
-    .use("/", express.static(__dirname));
+var app = express();
+app.use(logger({ format: 'dev'}));
+app.use("/", express.static(__dirname));
 
 console.log("http://localhost:3000");
 http.createServer(app).listen(3000);
